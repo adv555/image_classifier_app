adb4387057cd63cbea801f44d3d0f8d7c45df6a2
Olena Boldysheva <adv55577@gmail.com>
2023-04-02 01:32:28 +0300

    add cloudinary

--- image_classifier_app/settings.py
+++ image_classifier_app/settings.py
@@ -166,6 +166,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('API_KEY'),
+    'API_SECRET': os.environ.get('API_SECRET')
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
