6e642c742f72a3fb0969cf7ec17855b0ab9927cf
Olena Boldysheva <adv55577@gmail.com>
2023-04-01 23:53:21 +0300

    basic ui

---
+++ frontend/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'frontend'
+
+urlpatterns = [
+    path("", views.homepage, name="about"),
+    path("classifier", views.image_classifier, name="classifier"),
+    path("team", views.team_page, name="team"),
+    path('switch_theme', views.change_theme, name='change-theme')
+
+]
